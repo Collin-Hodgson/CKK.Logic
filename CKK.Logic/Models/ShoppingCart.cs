e31f79a2bf603d10d489d7479c77ffeff22afe0b
Collin <collinhodgs@gmail.com>
2022-02-25 10:28:30 -0700

    Created ShoppingCart.cs and added varibles, constructor, GetCustomerId, AddProduct, RemoveProduct

---
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -0,0 +1,30 @@
+﻿namespace CKK.Logic.Models
+{
+    class ShoppingCart
+    {
+        private Customer Customer;
+        private ShoppingCartItem Product1;
+        private ShoppingCartItem product2;
+        private ShoppingCartItem Product3;
+
+        public ShoppingCart(Customer customer)
+        {
+            Customer = customer;
+        }
+
+        public int GetCustomerId()
+        {
+            return Customer.GetId();
+        }
+
+        public ShoppingCartItem AddProduct(Product prod)
+        {
+            
+        }
+
+        public ShoppingCartItem RemoveProduct(Product prod, int quantity)
+        {
+
+        }
+    }
+}
