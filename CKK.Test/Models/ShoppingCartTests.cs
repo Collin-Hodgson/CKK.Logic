a0396736716b7c46142359add36e5215ea356622
Collin Hodgson <Collinhodgs@gmail.com>
2022-03-04 16:16:38 -0700

    Finished GetTotalSuccessfully unit test

--- CKK.Test/Models/ShoppingCartTests.cs
+++ CKK.Test/Models/ShoppingCartTests.cs
@@ -9,20 +9,20 @@ namespace CKK.Test.Models
         [Fact]
         public void AddProductSuccessfully()
         {
-                // Arrange
-                Customer testCustomer = new Customer();
-                Product testProduct = new Product();
-                ShoppingCart testCart = new ShoppingCart(testCustomer);
+            // Arrange
+            Customer testCustomer = new Customer();
+            Product testProduct = new Product();
+            ShoppingCart testCart = new ShoppingCart(testCustomer);
 
 
-                // Act
-                testProduct.SetId(1);
-                testCustomer.SetId(1);
+            // Act
+            testProduct.SetId(1);
+            testCustomer.SetId(1);
 
-                int Actual = testCart.AddProduct(testProduct, 2).GetQuantity();
+            int Actual = testCart.AddProduct(testProduct, 2).GetQuantity();
 
-                // Assert
-                Assert.Equal(2, Actual);
+            // Assert
+            Assert.Equal(2, Actual);
         }
 
         [Fact]
@@ -49,10 +49,29 @@ namespace CKK.Test.Models
         public void GetTotalSuccessfully()
         {
             // Arrange
+            Customer testCustomer = new Customer();
+            ShoppingCart testShopping = new ShoppingCart(testCustomer);
+            Product testProduct1 = new Product();
+            Product testProduct2 = new Product();
+            Product testProduct3 = new Product();
 
             // Act
+            testProduct1.SetId(1);
+            testProduct2.SetId(2);
+            testProduct3.SetId(3);
+
+            testProduct1.SetPrice(1);
+            testProduct2.SetPrice(1);
+            testProduct3.SetPrice(1);
+
+            testShopping.AddProduct(testProduct1, 2);
+            testShopping.AddProduct(testProduct2, 2);
+            testShopping.AddProduct(testProduct3, 2);
+
+            decimal Actual = testShopping.GetTotal();
 
             // Assert
+            Assert.Equal(6, Actual);
 
         }
     }
