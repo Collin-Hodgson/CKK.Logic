6bfdb66cd20a7374868e8f0bbd42e2d298c4afcb
Collin Hodgson <Collinhodgs@gmail.com>
2022-03-04 11:33:47 -0700

    Built XUnit Test methods

---
+++ CKK.Test/Models/ShoppingCartTests.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+
+namespace CKK.Test.Models
+{
+    public class ShoppingCartTests
+    {
+        [Fact]
+        public void AddProductSuccessfully()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+
+        [Fact]
+        public void RemoveProductSuccessfully()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+
+        [Fact]
+        public void GetTotalSuccessfully()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+    }
+}
